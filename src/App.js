2ceedb468c7d890b7013b457ff4659929fe1a4c2
Beril Ozbay <berilozbay97@gmail.com>
2022-07-06 09:49:10 +0300

    React state ve Event İncelendi

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import CategoryList from './CategoryList';
+import { Container, Row, Col } from 'reactstrap';
 //Baska component kullanılacağı zaman import edilir.
+import CategoryList from './CategoryList';
 import Navi from './Navi';
 import ProductList from './ProductList';
 
+
 function App() {
+  //reactstrapten container ve row yapıları alındı.
+  let productInfo= {title:"ProductList"};
+  let categoryInfo = {title:"CategoryList"};
   return (
     <div>
-      <Navi/>
-      <CategoryList/>
-      <ProductList/>
+      <Container>
+        <Row>
+          <Navi />
+        </Row>
+        <Row>
+          <Col xs="3"><CategoryList info={categoryInfo}/> </Col>
+          <Col xs="9"><ProductList info={productInfo} /></Col>
+        </Row>
+      </Container>
+
     </div>
   );
 }
